1850865c02851bc9d3a74378d66aaad223ee5613
Kah Wai <kahwai@healthmetrics.co>
2021-03-17 13:38:12 +0800

    Remove unnecessary files

--- lib/trust_fall.dart
+++ lib/trust_fall.dart
@@ -7,40 +7,36 @@ class TrustFall {
   static const MethodChannel _channel = const MethodChannel('trust_fall');
 
   //Checks whether device JailBroken on iOS/Android?
-  static Future<bool?> get isJailBroken async {
+  static Future<bool> get isJailBroken async {
     final bool? isJailBroken = await _channel.invokeMethod('isJailBroken');
-    return isJailBroken;
+    return isJailBroken!;
   }
 
   //Can this device mock location - no need to root!
-  static Future<bool?> get canMockLocation async {
+  static Future<bool> get canMockLocation async {
     final bool? canMockLocation =
         await _channel.invokeMethod('canMockLocation');
-    return canMockLocation;
+    return canMockLocation!;
   }
 
   // Checks whether device is real or emulator
-  static Future<bool?> get isRealDevice async {
+  static Future<bool> get isRealDevice async {
     final bool? isRealDevice = await _channel.invokeMethod('isRealDevice');
-    return isRealDevice;
+    return isRealDevice!;
   }
 
   // (ANDROID ONLY) Check if application is running on external storage
-  static Future<bool?> get isOnExternalStorage async {
+  static Future<bool> get isOnExternalStorage async {
     final bool? isOnExternalStorage =
         await _channel.invokeMethod('isOnExternalStorage');
-    return isOnExternalStorage;
+    return isOnExternalStorage!;
   }
 
   // Check if device violates any of the above
   static Future<bool> get isTrustFall async {
-    final bool? isJailBroken = await _channel.invokeMethod('isJailBroken');
-    final bool? canMockLocation =
-        await _channel.invokeMethod('canMockLocation');
-    final bool? isRealDevice = await _channel.invokeMethod('isRealDevice');
-
-    if (isJailBroken == null || canMockLocation == null || isRealDevice == null)
-      throw 'Could not determine whether your device is compromised';
+    final bool isJailBroken = await TrustFall.isJailBroken;
+    final bool canMockLocation = await TrustFall.canMockLocation;
+    final bool isRealDevice = await TrustFall.isRealDevice;
 
     if (Platform.isAndroid) {
       final bool? isOnExternalStorage =
